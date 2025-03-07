25af3b53ad6cb29f453c95d944e82f1f36a295cd
Curtis Cooper <curtiscooper@google.com>
2025-03-06 20:55:38 -0700

    Add GLM

--- src/TestMain.cpp
+++ src/TestMain.cpp
@@ -1,7 +1,24 @@
+#include <filesystem>
 #include <iostream>
 
+#include <glm/vec3.hpp>
+
+namespace {
+
+std::string getMeshTrianglesFilePath() {
+  std::string path = PROJECT_SOURCE_DIR;
+  path += "/mesh_triangles.txt";
+
+  return path;
+}
+
+
+
+} // namespace
+
 int main() {
-  std::cout << "Hello" << std::endl;
+  const std::string meshTrianglesFileFullPath = getMeshTrianglesFilePath();
+  std::cout << "Mesh Triangles File Path: " << meshTrianglesFileFullPath << std::endl;
 
   return 0;
 }
